9cce4f9201a436a83f302425616818fd352c2642
Datvite <tdinhdat09@gmail.com>
2025-09-24 15:11:50 +0700

    test

--- tons/cau1.cpp
+++ tons/cau1.cpp
@@ -1,40 +1,47 @@
-
 #include <bits/stdc++.h>
 #define skibidi                       \
     ios_base::sync_with_stdio(false); \
     cin.tie(0);                       \
     cout.tie(0);
-
-using namespace std;
-
+#define file(tenfile)                         \
+    if (fopen(tenfile ".inp", "r"))           \
+    {                                         \
+        freopen(tenfile ".inp", "r", stdin);  \
+        freopen(tenfile ".out", "w", stdout);  \
+    }
 #define int long long
-const int N = 1e5 + 5;
-int a[N];
-
-mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rand(int l, int h) /// hàm random từ l đến h
+#define fi first
+#define se second
+#define ii pair<int, int>
+#define iii pair<int, ii>
+#define On(mask, pos) (mask | (1LL << pos))
+#define Off(mask, pos) (mask ^ (1LL << pos))
+#define endl "\n"
+using namespace std;
+const int N = 1e6 + 69;
+const int BASE = 256;
+const int MOD = 2e9 + 11;
+int add(int a, int b)
 {
-    return uniform_int_distribution<int>(l, h)(rng);
+    return (a + b) % MOD;
 }
-
+int sub(int a, int b)
+{
+    return ((a - b) % MOD + MOD) % MOD;
+}
+int mul(int a, int b)
+{
+    return ((a % MOD) * (b % MOD)) % MOD;
+}
+int n, a[N];
+void solve()
+{
+    
+}
 main()
 {
-    srand(time(0));
-    skibidi for (int test = 1; test <= 100; test++)
-    {
-        ofstream cout(".inp");
-        int n = rand(1, 10), Q = rand(1, 10);
-        cout << n << endl;
-        /// a[1] = 1e9 - n + 1, a[2] = 1e9 - n + 2, ... , a[n] = 1e9
-        for (int i = 1; i <= n; i++)
-        {
-        }
-        system(".exe");
-        /// chạy code trâu
-        system(".exe");
-
-        /// fc = file compare
-        if (system("fc GAME08.out GAME08TRAU.out") == 1)
-            exit(0);
-    }
+    skibidi;
+    file("");
+    cin >> n;
+    solve();
 }
