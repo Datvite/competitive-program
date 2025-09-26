235cd17b98c05ef1d9ea86a0d57dec00895379bf
Datvite <tdinhdat09@gmail.com>
2025-09-26 15:17:32 +0700

    add

---
+++ binary/Minimum_maximum.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define skibidi                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define file(tenfile)                         \
+    if (fopen(tenfile ".inp", "r"))           \
+    {                                         \
+        freopen(tenfile ".inp", "r", stdin);  \
+        freopen(tenfile ".out", "w", stdout); \
+    }
+#define int long long
+#define fi first
+#define se second
+#define ii pair<int, int>
+#define iii pair<int, ii>
+#define On(mask, pos) (mask | (1LL << pos))
+#define Off(mask, pos) (mask ^ (1LL << pos))
+#define endl "\n"
+using namespace std;
+const int N = 1e6 + 69;
+const int BASE = 256;
+const int MOD = 2e9 + 11;
+int add(int a, int b)
+{
+    return (a + b) % MOD;
+}
+int sub(int a, int b)
+{
+    return ((a - b) % MOD + MOD) % MOD;
+}
+int mul(int a, int b)
+{
+    return ((a % MOD) * (b % MOD)) % MOD;
+}
+int a[N], n, k;
+bool check(int mid)
+{
+    int cnt = 0, sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += a[i];
+        if (sum == mid)
+        {
+            cnt++;
+            sum = 0;
+        }
+        else if (sum > mid)
+        {
+            cnt++;
+            sum = a[i];
+        }
+        if (cnt > k)
+            return 0;
+    }
+    if (sum > 0)
+        cnt++;
+    return cnt <= k;
+}
+void solve()
+{
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    int l = *max_element(a + 1, a + n + 1), r = 1e18, ans = 0;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (check(mid))
+        {
+            ans = mid, r = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
+    cout << ans;
+}
+main()
+{
+    skibidi;
+    file("");
+    cin >> n >> k;
+    solve();
+}
