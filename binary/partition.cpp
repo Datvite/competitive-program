d2492c2ed09296bc4190d7da1f2d4775ed47f681
Datvite <tdinhdat09@gmail.com>
2025-10-06 08:50:08 +0700

    add

---
+++ binary/partition.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define skibidi                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define file(tenfile)                         \
+    if (fopen(tenfile ".inp", "r"))           \
+    {                                         \
+        freopen(tenfile ".inp", "r", stdin);  \
+        freopen(tenfile ".out", "w", stdout); \
+    }
+#define int long long
+#define fi first
+#define se second
+#define ii pair<int, int>
+#define iii pair<int, ii>
+#define On(mask, pos) (mask | (1LL << pos))
+#define Off(mask, pos) (mask ^ (1LL << pos))
+#define endl "\n"
+using namespace std;
+const int N = 1e6 + 69;
+const int BASE = 256;
+const int MOD = 2e9 + 11;
+int add(int a, int b)
+{
+    return (a + b) % MOD;
+}
+int sub(int a, int b)
+{
+    return ((a - b) % MOD + MOD) % MOD;
+}
+int mul(int a, int b)
+{
+    return ((a % MOD) * (b % MOD)) % MOD;
+}
+int n, a[N], sum = 0, t, l;
+priority_queue<int, vector<int>, greater<int>> pq;
+void solve()
+{
+    cin >> l >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        pq.push(x);
+        sum += x;
+    }
+    if (l > sum)
+        pq.push(l - sum);
+    int res = 0;
+    while (pq.size() > 1)
+    {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        pq.push(a + b);
+        res += a + b;
+    }
+    cout << res << endl;
+}
+main()
+{
+    skibidi;
+    file("partition");
+    solve();
+}
